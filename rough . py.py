0fd3b216dea26aba8177dbfd93bc0026d15c3cfc
Dhiren-creator09 <dhirenkumar9908@gmail.com>
2026-01-09 15:22:58 +0530

    Add files via upload

---
+++ rough . py.py
@@ -0,0 +1,12 @@
+print("ddhiren next project rough")
+favouritegod = str(input("enter your favourite chaos god:"))
+if favouritegod.lower() == "khorne":
+    print("blood for the blood god")
+elif favouritegod.lower() == "nurgle":
+    print("for the grandfather nurgle")   
+elif favouritegod.lower() == "slannesh":
+    print("perfection")
+else:
+    print("change for the sake of change")
+
+   
